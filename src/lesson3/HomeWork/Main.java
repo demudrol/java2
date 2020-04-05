0e8695da9134032213b2fc6436e1ca14393a32a9
Nikolay Dementyev <nikolay.v.dementyev@megafon.ru>
2020-04-05 19:08:12 +0300

    that's it

--- src/lesson3/HomeWork/Main.java
+++ src/lesson3/HomeWork/Main.java
@@ -5,11 +5,11 @@ public class Main {
     public static void main(String[] args) {
         AddressBook addr = new AddressBook();
 
-        addr.add(112, "ololo");
-        addr.add(811,"rolo");
-        addr.add(811123,"rolo");
+        addr.add(112, "pizza");
+        addr.add(811,"clinic");
+        addr.add(811123,"pizza");
 
-        System.out.println(addr.get("rolo"));
+        System.out.println(addr.get("pizza"));
 
 
 
